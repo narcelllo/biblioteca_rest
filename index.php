4e2983d41180e9232a929151e142fc7ee0040e68
Marcelo <santosmarcelo1995@live.com>
2021-10-18 22:11:40 -0300

    Add router to index.php

--- index.php
+++ index.php
@@ -1,11 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <div>Hello World</div>
-</body>
-</html>
+<?php
+    require './db/db.php';
+
+    $routes = array(
+        '/',
+        '/users',
+        '/books'
+    );
+
+    function router($routes, $conn) {
+        foreach ($routes as $path) {
+            if (isset($_SERVER['PATH_INFO'])) {
+                $endpoint = $_SERVER['PATH_INFO'];
+            } else {
+                $endpoint = '/';
+            }
+
+            if ($path == $endpoint) {
+                search($path, $conn);
+                return;
+            }
+        }
+
+        mysqli_close($conn);
+        echo 'Sorry! Page not found';
+    }
+
+    router($routes, $conn);
+?> 
